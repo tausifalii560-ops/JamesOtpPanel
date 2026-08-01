365c253f167c9874b257ff0d4514e3b22f4e809c
tausifalii560-ops <tausifalii560@gmail.com>
2026-08-02 04:17:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,18 @@
 import asyncio
 
 from aiogram import Bot, Dispatcher
-from handlers.start import router as start_router
+from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
+from aiogram.filters import CommandStart
+from aiogram.types import Message
 
 from config import BOT_TOKEN
 
-bot = Bot(token=BOT_TOKEN)
+bot = Bot(
+    token=BOT_TOKEN,
+    default=DefaultBotProperties(
+        parse_mode=ParseMode.HTML
+    )
+)
+
 dp = Dispatcher()
-
-dp.include_router(start_router)
-
-
-async def main():
-    await dp.start_polling(bot)
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
